f8d97a7078c915561be30200e43cf2d023b557fd
TreesZendesk <diastowo@gmail.com>
2020-06-18 17:05:01 +0700

    test raspi-serial

--- test-serial.js
+++ test-serial.js
@@ -4,9 +4,12 @@ const Serial = require('raspi-serial').Serial;
 raspi.init(() => {
   var serial = new Serial();
   serial.open(() => {
+    console.log('serial opening')
     serial.on('data', (data) => {
+        console.log(data)
       process.stdout.write(data);
     });
+    console.log('sending data')
     serial.write('Hello from raspi-serial');
   });
 });
