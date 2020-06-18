2abed2ca5e2bbfb0844c0a246a51705fc7bde3d6
TreesZendesk <diastowo@gmail.com>
2020-06-18 17:01:48 +0700

    test raspi-serial

--- test-serial.js
+++ test-serial.js
@@ -1,23 +1,12 @@
-var SerialPort = require('serialport');
+const raspi = require('raspi');
+const Serial = require('raspi-serial').Serial;
 
-var com = new SerialPort("/dev/cu.usbmodem14131", {
-    baudRate: 9600,
-    databits: 8,
-    parity: 'none'
-}, false);
-
-com.open(function (error) {
-    if (error) {
-        console.log('Error while opening the port ' + error);
-    } else {
-        console.log('CST port open');
-        com.write(1, function (err, result) {
-            if (err) {
-                console.log('Error while sending message : ' + err);
-            }
-            if (result) {
-                console.log('Response received after sending message : ' + result);
-            }    
-        });
-    }              
+raspi.init(() => {
+  var serial = new Serial();
+  serial.open(() => {
+    serial.on('data', (data) => {
+      process.stdout.write(data);
+    });
+    serial.write('Hello from raspi-serial');
+  });
 });
